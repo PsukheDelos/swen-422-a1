2543caa14d874c2a2348607bf38aa4b854e5697c
Glen Peek <glen.peek@gmail.com>
2015-08-11 21:08:51 +1200

    changed the way some gestures work

--- js/script.js
+++ js/script.js
@@ -2,135 +2,266 @@
 var previousFrame = null;
 var paused = false;
 var pauseOnGesture = false;
+var appX = 0;
+var appY = 0;
 
 // Setup Leap loop with frame callback function
 var controllerOptions = {enableGestures: true};
 
 Leap.loop(controllerOptions, function(frame) {
 
-  if($('#selected').size() > 0) {
+  try{
+    var appWidth = document.body.clientWidth;
+    var appHeight = document.body.clientHeight;
 
-    // Display Hand object data
-    var handString = "No hands";
-    if (frame.hands.length == 2) {
-      for (var i = 0; i < frame.hands.length; i++) {
-        var hand = frame.hands[i];
-        var img = $("#selected");
-        // if hand roll detected
-        if (hand.roll() < -0.5) { $('#gallery').tooltip('rotate', img, "right"); }
-        else if (hand.roll() > 0.5) { $('#gallery').tooltip('rotate', img, "left"); }
-        // if hand z change detected
-        if (previousFrame != null) {
-          var hChange = hand.translation(previousFrame);
-          if (hChange[2] > 3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); }
-          else if (hChange[2] < -3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); }
-          // move the photo when selected
-          var position = $('#selected').position();
-          var xChange = position.left; var yChange = position.top;
-          if (hChange[0] > 1) xChange+=50;
-          else if (hChange[0] < -1) xChange-=50;
-          if (hChange[1] < -1) yChange+=50;
-          else if (hChange[1] > 1) yChange-=50;
-          $('#selected').css({top: yChange, left: xChange});
-        }
-      }
-    }
-    // Display Gesture object data
-    var gestureString = "No gestures";
-    if (frame.gestures.length > 0 && frame.hands.length==2) {
-      if (pauseOnGesture) { togglePause(); }
-      for (var i = 0; i < frame.gestures.length; i++) {
-        var gesture = frame.gestures[i];
-        // find gesture type
-        switch (gesture.type) {
-          case "circle":
-            // gestureString = "circle"
-            var clockwise = false;
-            var pointableID = gesture.pointableIds[0];
-            var direction = frame.pointable(pointableID).direction;
-            var dotProduct = Leap.vec3.dot(direction, gesture.normal);
-            if(dotProduct > 0) clockwise = true;
-            var img = $("#selected")
-            if(clockwise){ $('#gallery').tooltip('scale', img, "increase"); }
-            else{ $('#gallery').tooltip('scale', img, "decrease"); }
-            break;
-          case "swipe": gestureString  = "swipe"; break;
-          case "screenTap": gestureString  = "screenTap"; break;
-          case "keyTap": tapKey(); break;
-          default: break;
-        }
-      }
-    }
+    var iBox = frame.interactionBox;
+    var pointable = frame.pointables[0];
 
-    if (frame.hands.length==1 && frame.gestures.length == 0){
-      try{
-        var appWidth = document.body.clientWidth;
-        var appHeight = document.body.clientHeight;
+    var leapPoint = pointable.stabilizedTipPosition;
+    var normalizedPoint = iBox.normalizePoint(leapPoint, true);
 
-        var iBox = frame.interactionBox;
-        var pointable = frame.pointables[0];
+    appX = normalizedPoint[0] * appWidth;
+    appY = (1 - normalizedPoint[1]) * appHeight;
 
-        var leapPoint = pointable.stabilizedTipPosition;
-        var normalizedPoint = iBox.normalizePoint(leapPoint, true);
+    // console.log("touchzone: " + touchZone);
+    var touchZone = frame.pointables[0].touchZone;
 
-        var appX = normalizedPoint[0] * appWidth;
-        var appY = (1 - normalizedPoint[1]) * appHeight;
+    if (!(touchZone=="hovering"  && $('#selected').size() > 0)){
+          moveSection("target", appX, appY);
+    } 
 
-        var img = $('#selected').children('img').attr('src');
-        var cssObj = {'left' : appX, 'top' : appY};
-        $('#selected').css(cssObj);
+    if(frame.pointables.length > 0 && frame.hands.length==1)
+    {
+        if(touchZone=="touching" && $('#selected').size() == 0){
+          console.log('touching');
+          $(document.elementFromPoint(appX, appY)).click();
 
-        console.log("X: " + appX);
-        console.log("Y: " + appY);
+        }else if (touchZone=="touching" && $('#selected').size() > 0){
+          console.log('move');
+          var cssObj = {'left' : appX-$('#selected').width()-40, 'top' : (appY-$('#selected').height()/2)};
+          $('#selected').css(cssObj);
+          $('#selected').css({ background: "red" });
+          $('#target').css({ fill: "red" });
 
-      }catch(e){
-        functionToHandleError(e);
-      }
-    }
+        }else if (touchZone=="hovering"  && $('#selected').size() > 0){
+          console.log('hovering');
+          $('#target').css({ fill: "green" });
+          $('#selected').css({ background: "green" });
+          
+          var handString = "No hands";
+          // for (var i = 0; i < frame.hands.length; i++) {
+            var hand = frame.hands[0];
+            var img = $("#selected");
+            // if hand roll detected
+            if (hand.roll() < -0.5) { $('#gallery').tooltip('rotate', img, "right"); }
+            else if (hand.roll() > 0.5) { $('#gallery').tooltip('rotate', img, "left"); }
+            // if hand z change detected
+            // if (previousFrame != null) {
+            //   var hChange = hand.translation(previousFrame);
+            //   if (hChange[2] > 3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); }
+            //   else if (hChange[2] < -3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); }
+            //   // move the photo when selected
+            //   var position = $('#selected').position();
+            //   var xChange = position.left; var yChange = position.top;
+            //   if (hChange[0] > 1) xChange+=50;
+            //   else if (hChange[0] < -1) xChange-=50;
+            //   if (hChange[1] < -1) yChange+=50;
+            //   else if (hChange[1] > 1) yChange-=50;
+            //   $('#selected').css({top: yChange, left: xChange});
+            // }
+          // }
+          
 
-
-    // Store frame for motion functions
-    // if (gestureString != "No gestures") { console.log(gestureString); }
-
-  // Store frame for motion functions
-  previousFrame = frame;
-
-  }else{
-    console.log('nothing selected');
-    var gestureString = "No gestures";
-    if (frame.gestures.length > 0 && frame.hands.length==1) {
-      if (pauseOnGesture) { togglePause(); }
-      for (var i = 0; i < frame.gestures.length; i++) {
-        var gesture = frame.gestures[i];
-        // find gesture type
-        switch (gesture.type) {
-          case "keyTap": 
-            gestureString = "keyTap"; 
-            try{
-              var appWidth = document.body.clientWidth;
-              var appHeight = document.body.clientHeight;
-
-              var iBox = frame.interactionBox;
-              var pointable = frame.pointables[0];
-
-              var leapPoint = pointable.stabilizedTipPosition;
-              var normalizedPoint = iBox.normalizePoint(leapPoint, true);
-
-              var appX = normalizedPoint[0] * appWidth;
-              var appY = (1 - normalizedPoint[1]) * appHeight;
-
-              $(document.elementFromPoint(appX, appY)).click();
-              console.log ("X: " + appX + ", Y: " + appY);
-              console.log("click");
-            }catch(e){
-              functionToHandleError(e);
+          if (pauseOnGesture) { togglePause(); }
+          var gestureString = "No gestures";
+          if (pauseOnGesture) { togglePause(); }
+          for (var i = 0; i < frame.gestures.length; i++) {
+            var gesture = frame.gestures[i];
+            switch (gesture.type) {
+              case "circle":
+                gestureString = "circle";
+                var clockwise = false;
+                var pointableID = gesture.pointableIds[0];
+                var direction = frame.pointable(pointableID).direction;
+                var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+                if(dotProduct > 0) clockwise = true;
+                var img = $("#selected")
+                if(clockwise){ $('#gallery').tooltip('scale', img, "increase"); }
+                else{ $('#gallery').tooltip('scale', img, "decrease"); }
+                break;
+              case "swipe": gestureString  = "swipe"; break;
+              case "screenTap": gestureString  = "screenTap"; break;
+              case "keyTap": tapKey(); break;
+              default: break;
             }
-            break;
-          default: break;
+          }
+
+        }else {
+            $('#selected').css({ background: "white" });
+            $("#selected").attr('id', '');
+            $('#target').css({ fill: "grey" });
         }
-      }
+    }else if(frame.pointables.length > 0 && frame.hands.length==2 && touchZone=="hovering"  && $('#selected').size() > 0){
+        console.log("zupdate");
+          var hand = frame.hands[1];
+          var img = $("#selected");
+          // if hand z change detected
+          if (previousFrame != null) {
+            var hChange = hand.translation(previousFrame);
+            console.log(hChange);
+            if (hChange[2] > 0.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); console.log("up");}
+            else if (hChange[2] < -0.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); console.log("down");}
+          }
     }
+    // else {
+    //   $('#selected').css({ background: "white" });
+    //   $("#selected").attr('id', '');
+    //   $('#target').css({ fill: "grey" });
+    // }
+
+  }catch(e){
+    // console.log("error");
   }
+
+
+
+  // if($('#selected').size() > 0) {
+  //   // Display Hand object data
+  //   var handString = "No hands";
+  //   if (frame.hands.length == 2) {
+  //     for (var i = 0; i < frame.hands.length; i++) {
+  //       var hand = frame.hands[i];
+  //       var img = $("#selected");
+  //       // if hand roll detected
+  //       if (hand.roll() < -0.5) { $('#gallery').tooltip('rotate', img, "right"); }
+  //       else if (hand.roll() > 0.5) { $('#gallery').tooltip('rotate', img, "left"); }
+  //       // if hand z change detected
+  //       if (previousFrame != null) {
+  //         var hChange = hand.translation(previousFrame);
+  //         if (hChange[2] > 3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); }
+  //         else if (hChange[2] < -3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); }
+  //         // move the photo when selected
+  //         var position = $('#selected').position();
+  //         var xChange = position.left; var yChange = position.top;
+  //         if (hChange[0] > 1) xChange+=50;
+  //         else if (hChange[0] < -1) xChange-=50;
+  //         if (hChange[1] < -1) yChange+=50;
+  //         else if (hChange[1] > 1) yChange-=50;
+  //         $('#selected').css({top: yChange, left: xChange});
+  //       }
+  //     }
+  //   }
+  //   // Display Gesture object data
+  //   var gestureString = "No gestures";
+  //   if (frame.gestures.length > 0 && frame.hands.length==2) {
+  //     if (pauseOnGesture) { togglePause(); }
+  //     for (var i = 0; i < frame.gestures.length; i++) {
+  //       var gesture = frame.gestures[i];
+  //       // find gesture type
+  //       switch (gesture.type) {
+  //         case "circle":
+  //           // gestureString = "circle"
+  //           var clockwise = false;
+  //           var pointableID = gesture.pointableIds[0];
+  //           var direction = frame.pointable(pointableID).direction;
+  //           var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+  //           if(dotProduct > 0) clockwise = true;
+  //           var img = $("#selected")
+  //           if(clockwise){ $('#gallery').tooltip('scale', img, "increase"); }
+  //           else{ $('#gallery').tooltip('scale', img, "decrease"); }
+  //           break;
+  //         case "swipe": gestureString  = "swipe"; break;
+  //         case "screenTap": gestureString  = "screenTap"; break;
+  //         case "keyTap": tapKey(); break;
+  //         default: break;
+  //       }
+  //     }
+  //   }
+
+  //   // if (frame.gestures.length > 0 && frame.hands.length==1) {
+  //   //   if (pauseOnGesture) { togglePause(); }
+  //   //   for (var i = 0; i < frame.gestures.length; i++) {
+  //   //     var gesture = frame.gestures[i];
+  //   //     // find gesture type
+  //   //     switch (gesture.type) {
+  //   //       case "keyTap": 
+  //   //         gestureString = "keyTap"; 
+  //   //         try{
+  //   //           var appWidth = document.body.clientWidth;
+  //   //           var appHeight = document.body.clientHeight;
+
+  //   //           var iBox = frame.interactionBox;
+  //   //           var pointable = frame.pointables[0];
+
+  //   //           var leapPoint = pointable.stabilizedTipPosition;
+  //   //           var normalizedPoint = iBox.normalizePoint(leapPoint, true);
+
+  //   //           var appX = normalizedPoint[0] * appWidth;
+  //   //           var appY = (1 - normalizedPoint[1]) * appHeight;
+
+  //   //           console.log ("X: " + appX + ", Y: " + appY);
+  //   //           console.log("click");
+  //   //           $("#selected").attr('id', '');
+  //   //           $(document.elementFromPoint(appX, appY)).click();
+
+  //   //         }catch(e){
+  //   //           functionToHandleError(e);
+  //   //         }
+  //   //         break;
+  //   //       default: break;
+  //   //     }
+  //   //   }
+  //   // }    
+
+  //   // if (frame.hands.length==1 && frame.gestures.length == 0){
+  //   //   try{
+  //   //     var appWidth = document.body.clientWidth;
+  //   //     var appHeight = document.body.clientHeight;
+
+  //   //     var iBox = frame.interactionBox;
+  //   //     var pointable = frame.pointables[0];
+
+  //   //     var leapPoint = pointable.stabilizedTipPosition;
+  //   //     var normalizedPoint = iBox.normalizePoint(leapPoint, true);
+
+  //   //     var appX = normalizedPoint[0] * appWidth;
+  //   //     var appY = (1 - normalizedPoint[1]) * appHeight;
+
+  //   //     var img = $('#selected').children('img').attr('src');
+  //   //     var cssObj = {'left' : appX, 'top' : appY};
+  //   //     $('#selected').css(cssObj);
+
+  //   //     console.log("X: " + appX);
+  //   //     console.log("Y: " + appY);
+
+  //   //   }catch(e){
+  //   //     functionToHandleError(e);
+  //   //   }
+  //   // }
+
+
+  //   // Store frame for motion functions
+  //   // if (gestureString != "No gestures") { console.log(gestureString); }
+
+  // // Store frame for motion functions
+  // // previousFrame = frame;
+
+  // }else{
+  //   console.log("Else");
+  //   var gestureString = "No gestures";
+  //   if (frame.gestures.length > 0 && frame.hands.length==1) {
+  //     if (pauseOnGesture) { togglePause(); }
+  //     for (var i = 0; i < frame.gestures.length; i++) {
+  //       var gesture = frame.gestures[i];
+  //       // find gesture type
+  //       switch (gesture.type) {
+  //         case "keyTap": gestureString = "keyTap"; console.log("keytap"); break;
+  //         default: break;
+  //       }
+  //     }
+  //   }
+  // }
+  previousFrame = frame;
 });
 
 
@@ -177,4 +308,12 @@ function toggleFullScreen() {
     else if (document.mozCancelFullScreen) { document.mozCancelFullScreen(); } 
     else if (document.webkitCancelFullScreen) { document.webkitCancelFullScreen(); }  
   }  
+}
+
+function moveSection(idStr, xOffset, yOffset) {
+var domElemnt = document.getElementById(idStr);
+    if (domElemnt) {
+        var transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
+        domElemnt.setAttribute('transform', transformAttr);
+    }
 }
