6980fd125ce2022a2d2f6ea236b74a0e75ca1920
mccaulcall <mccaulcall@gmail.com>
2015-08-17 16:05:25 +1200

    added swipe in from thumbs

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ var appY = 0;
 
 var swipeUp = 0;
 var swipeDn = 0;
+var swipeRt = 0;
+var swipeLf = 0;
 
 // Setup Leap loop with frame callback function
 var controllerOptions = {enableGestures: true};
@@ -33,25 +35,43 @@ Leap.loop(controllerOptions, function(frame) {
           moveSection("target", appX, appY);
     } 
 
-    if(frame.pointables.length > 0 && frame.hands.length==1)
-    {
-        console.log(document.elementFromPoint(appX, appY)));
-        if(touchZone=="touching" && $('#selected').size() == 0){
+    if(frame.pointables.length > 0 && frame.hands.length==1) {
+        // console.log(document.elementFromPoint(appX, appY));
+        if(touchZone!="touching" && $('#selected').size() == 0) {
+          for (var i = 0; i < frame.gestures.length; i++) {
+            // $("#gallery-ul").append($("<li><img src=\"img/" + 2 + ".jpg\" alt=\"\"></img></li>"));
+            // $('#gallery').tooltip('init', $("#selected"), "down");
+            var gestureo = frame.gestures[i];
+            switch (gestureo.type) {
+              case "swipe":
+                // console.log("no selected & swipe");
+                gestureString  = "swipe"; 
+                var isHorizontal = Math.abs(gestureo.direction[0]) > Math.abs(gestureo.direction[1]);
+                if(isHorizontal) {
+                  if(gestureo.direction[0] > 0) {swipeRt++;} // right swipe
+                  else {swipeLf++;} // left swipe
+                }
+                break;
+              default: break;
+            }
+            if (swipeRt > 12) {
+              addImages();
+              swipeRt = 0;
+            }
+          }
+        } else if(touchZone=="touching" && $('#selected').size() == 0) {
           // console.log('touching');
           $(document.elementFromPoint(appX, appY)).click();
-
-        }else if (touchZone=="touching" && $('#selected').size() > 0){
+        } else if (touchZone=="touching" && $('#selected').size() > 0) {
           // console.log('move');
           var cssObj = {'left' : appX-$('#selected').width()-40, 'top' : (appY-$('#selected').height()/2)};
           $('#selected').css(cssObj);
           $('#selected').css({ background: "red" });
           $('#target').css({ fill: "red" });
-
-        }else if (touchZone=="hovering"  && $('#selected').size() > 0){
+        } else if (touchZone=="hovering"  && $('#selected').size() > 0) {
           // console.log('hovering');
           $('#target').css({ fill: "green" });
           $('#selected').css({ background: "green" });
-          
           var handString = "No hands";
           // for (var i = 0; i < frame.hands.length; i++) {
             var hand = frame.hands[0];
@@ -59,21 +79,6 @@ Leap.loop(controllerOptions, function(frame) {
             // if hand roll detected
             if (hand.roll() < -0.5) { $('#gallery').tooltip('rotate', img, "right"); }
             else if (hand.roll() > 0.5) { $('#gallery').tooltip('rotate', img, "left"); }
-            // if hand z change detected
-            // if (previousFrame != null) {
-            //   var hChange = hand.translation(previousFrame);
-            //   if (hChange[2] > 3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); } # Tried a few values. 2/2.5 seems a bit more responsive.
-            //   else if (hChange[2] < -3.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); }
-            //   // move the photo when selected
-            //   var position = $('#selected').position();
-            //   var xChange = position.left; var yChange = position.top;
-            //   if (hChange[0] > 1) xChange+=50;
-            //   else if (hChange[0] < -1) xChange-=50;
-            //   if (hChange[1] < -1) yChange+=50;
-            //   else if (hChange[1] > 1) yChange-=50;
-            //   $('#selected').css({top: yChange, left: xChange});
-            // }
-          // }
 
           if (pauseOnGesture) { togglePause(); }
           var gestureString = "No gestures";
@@ -96,21 +101,20 @@ Leap.loop(controllerOptions, function(frame) {
                 gestureString  = "swipe"; 
                 var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
                 if(isHorizontal) {
-                  if(gesture.direction[0] > 0) {} // left swipe
-                  else {} // right swipe
+                  if(gesture.direction[0] > 0) {console.log("left");} // left swipe
+                  else {shitHappens();console.log("right");} // right swipe
                 } else { //vertical
                   if(gesture.direction[1] > 0) {
-                    //console.log(swipeUp);
+                    console.log(swipeUp);
                     swipeUp++;
-                    if(swipeUp > 50) {
+                    if(swipeUp > 40) {
                       swipeUp = 0;
-
                       $('#gallery').tooltip('zUpdate', $("#selected"), "up");
                     }
                   } else {
-                    //console.log(swipeDn);
+                    console.log(swipeDn);
                     swipeDn++;
-                    if(swipeDn > 50) {
+                    if(swipeDn > 40) {
                       swipeDn = 0;
                       $('#gallery').tooltip('zUpdate', $("#selected"), "down");
                     }
@@ -119,41 +123,19 @@ Leap.loop(controllerOptions, function(frame) {
                 break;
               case "screenTap": gestureString  = "screenTap"; break;
               case "keyTap": 
-                //$('#gallery').tooltip('zUpdate', $("#selected"), "cycle"); 
-                // console.log("down");
+                $('#gallery').tooltip('zUpdate', $("#selected"), "cycle"); 
+                // console.log("cycleZ");
                 break;
               default: break;
             }
           }
 
-        }else {
+        } else {
             $('#selected').css({ background: "white" });
             $("#selected").attr('id', '');
             $('#target').css({ fill: "grey" });
         }
     }
-    //This is how Z-Update works, but haven't quite figured out best way
-    //This was an experiment using to hands.  One holds the item, while the other hand moves forward and back to change Z
-    //Trying out a keytap cycle as an experiment
-    
-    // else if(frame.pointables.length > 0 && frame.hands.length==2 && touchZone=="hovering"  && $('#selected').size() > 0){
-    //     console.log("zupdate");
-    //       var hand = frame.hands[1];
-    //       var img = $("#selected");
-    //       // if hand z change detected
-    //       if (previousFrame != null) {
-    //         var hChange = hand.translation(previousFrame);
-    //         console.log(hChange);
-    //         if (hChange[2] > 0.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "up"); console.log("up");}
-    //         else if (hChange[2] < -0.5) { $('#gallery').tooltip('zUpdate', $("#selected"), "down"); console.log("down");}
-    //       }
-    // }
-    // else {
-    //   $('#selected').css({ background: "white" });
-    //   $("#selected").attr('id', '');
-    //   $('#target').css({ fill: "grey" });
-    // }
-
   }catch(e){
     // console.log("error");
   }
@@ -162,7 +144,7 @@ Leap.loop(controllerOptions, function(frame) {
 
 
 //load in all images from img folder  
-for(x = 0 ; x<4 ; x++){ $("#gallery-ul").append($("<li><img src=\"img/" + x + ".jpg\" alt=\"\"></img></li>")); }
+// for(x = 0 ; x<4 ; x++){ $("#gallery-ul").append($("<li><img src=\"img/" + x + ".jpg\" alt=\"\"></img></li>")); }
 
 //create photo gallery
 $(function() { $('#gallery').b1njPolaroidGallery(); });
@@ -196,4 +178,13 @@ var domElemnt = document.getElementById(idStr);
         var transformAttr = ' translate(' + xOffset + ',' + yOffset + ')';
         domElemnt.setAttribute('transform', transformAttr);
     }
+}
+
+function addImages() { // on right swipe with no images being moved or manipulated, add some images to work with
+  // console.log($(".selected-thumb").length);
+  $(".selected-thumb").each(function(){
+    // console.log($(this).attr('id'));
+    $("#gallery-ul").append($("<li><img src=\"img/" + $(this).attr('id') + ".jpg\" alt=\"\"></img></li>"));
+  });
+  $('#gallery').tooltip('init', $("#selected"), "down");
 }
